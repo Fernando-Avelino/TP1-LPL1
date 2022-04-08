c4990c1871a64a669d15174ca6b6677651df7e5b
Fernando-Avelino <62761892+Fernando-Avelino@users.noreply.github.com>
2022-04-08 19:47:39 -0300

    Update TP01Ex16.java

--- TP01Ex16.java
+++ TP01Ex16.java
@@ -10,6 +10,8 @@ import java.util.Scanner;
  16. Entrar via teclado com o valor de um ângulo em graus, calcular e exibir as 
  seguintes funções trigonométricas: seno, cosseno, tangente e secante deste 
  ângulo. Lembre-se que uma função trigonométrica trabalha em radianos. 
+ Autores: Fernando Avelino 
+	         Vinícius Nunes
  */
 public class TP01Ex16 {
     public static void main(String[] args) {
