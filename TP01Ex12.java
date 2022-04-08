294061526f1441b403710182802c6f3928b0f647
Fernando-Avelino <62761892+Fernando-Avelino@users.noreply.github.com>
2022-04-08 19:46:16 -0300

    Update TP01Ex12.java

--- TP01Ex12.java
+++ TP01Ex12.java
@@ -9,6 +9,8 @@ import java.util.Scanner;
 /**
  12. Calcular e exibir o volume de um cone a partir dos valores da altura e do raio 
  da base que serão digitados.
+ Autores: Fernando Avelino 
+	  Vinícius Nunes
  */
 public class TP01Ex12 {
     public static void main(String[] args) {
