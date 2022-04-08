bb4963270a1ee251caf0992e7c21af77ad8d5126
Fernando-Avelino <62761892+Fernando-Avelino@users.noreply.github.com>
2022-04-08 19:51:25 -0300

    Update TP01Ex09.java

--- TP01Ex09.java
+++ TP01Ex09.java
@@ -10,8 +10,9 @@ import java.util.Scanner;
  9. Calcular e exibir a tensão de um determinado circuito eletrônico a partir dos
  valores da resistência e corrente elétrica que serão digitados. Utilize a lei
  de Ohm. 
+ 
  Autores: Fernando Avelino 
-	         Vinícius Nunes
+	  Vinícius Nunes
  */
 public class TP01Ex09 {
     public static void main(String args[]){
