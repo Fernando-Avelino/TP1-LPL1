9211312883f3c902f3d3296882cb5cd92343540f
Fernando-Avelino <62761892+Fernando-Avelino@users.noreply.github.com>
2022-04-08 19:48:52 -0300

    Update TP01Ex18.java

--- TP01Ex18.java
+++ TP01Ex18.java
@@ -10,7 +10,10 @@ import java.util.Scanner;
  18. Entrar via teclado com o valor de cinco produtos. Após as entradas, digitar
  um valor referente ao pagamento da somatória destes valores. Calcular e exibir
  o troco que deverá ser devolvido.
- */
+
+Autores: Fernando Avelino 
+	 Vinícius Nunes
+*/
 public class TP01Ex18 {
     public static void main(String args[]){
     
