80c40d8a626f0eed15c5c975121520631e1b7741
Fernando-Avelino <62761892+Fernando-Avelino@users.noreply.github.com>
2022-04-08 19:49:32 -0300

    Update TP01Ex03.java

--- TP01Ex03.java
+++ TP01Ex03.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 
 /**
  3. Calcular e exibir a área de um quadrado a partir do valor de sua diagonal que será digitado. 
+ 
  Autores: Fernando Avelino 
 	  Vinícius Nunes
  */
